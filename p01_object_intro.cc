9b820bbc95c73b86bed3fafba6c2752ec713d734
ITKevin-Tsai <it.kevintsai@gmail.com>
2025-04-23 17:29:14 +0800

    Update p01_object_intro.cc

--- p01_object_intro.cc
+++ p01_object_intro.cc
@@ -1,30 +1,30 @@
-#include <iostream>
+#include <cstring> // for strcpy
 
 class Person
 {
-public:
-
-	void input()
-	{
-		std::cin.getline(name, 80); // same as C-language fgets(name, 80, stdin);
-		std::cin >> age; // same as C-language scanf("%d", &age);
-		std::cin.ignore(); // // same as C-language fflush(stdin);
-	}
-	void output()
-	{
-		std::cout << "Name: " << name << std::endl << "Age: " << age << std::endl;
-	}
 	char name[80];
 	int age;
+public:
+	Person()  // Constructor
+	{
+		name[0] = '\0'; // Initialize to empty string
+		age = 0;
+	}
+	~Person() {}  // De-Constructor
+
+	void input();
+	void output();
 };
 
-
-int main()
+void Person::input()
 {
-	Person p1, p2;
-	p1.input();
-	p2.input();
-	p1.output();
-	p2.output();
-	return 0;
+	std::cin.getline(name, 80); // fgets(name, 80, stdin);
+	std::cin >> age; // scanf("%d%, &age);
+	std::cin.ignore(); // fflush(stdin);
+}
+
+void Person::output()
+{
+	std::cout << "Name: " << name << std::endl;
+	std::cout << "Age: " << age << std::endl;
 }
